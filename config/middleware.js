b58263d86d852da3d0bd47ca56a5bec7b14d6fdc
Robert Gühne <robert@polyxostudios.de>
2020-12-17 17:52:22 +0100

    fix(Strapi): upgrading strapi

--- config/middleware.js
+++ config/middleware.js
@@ -1,16 +1,11 @@
 module.exports = {
-  timeout: 100,
-  load: {
-    before: ["responseTime", "logger", "cors", "responses", "gzip"],
-    order: [
-      "Define the middlewares' load order by putting their name in this array is the right order"
-    ],
-    after: ["parser", "router"]
-  },
   settings: {
-    public: {
-      path: "./public",
-      maxAge: 60000
-    }
-  }
+    parser: {
+      enabled: true,
+      multipart: true,
+      formidable: {
+        maxFileSize: 5 * 1024 * 1024 * 1024,
+      },
+    },
+  },
 };
